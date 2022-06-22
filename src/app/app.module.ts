de4f7fca36508354ee9f98efd584437aab70c4f4
Edward <edward.neto@triersistemas.com.br>
2022-06-22 14:22:54 -0300

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { MenuModule } from './menu/menu.module';
 import { SobreModule } from './sobre/sobre.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, SobreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    SobreModule,
+    MenuModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
